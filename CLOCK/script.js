1e508acae192d64a2a1418101245f00955bad983
ShaikSameera-design <shaiksameera2624@gmail.com>
2025-02-28 22:24:31 +0000

    AnalogClock

---
+++ CLOCK/script.js
@@ -0,0 +1,21 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours() % 12;
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourAngle = (hours + minutes / 60) * 30;
+    const minuteAngle = minutes * 6;
+    const secondAngle = seconds * 6;
+
+    hourHand.style.transform = `rotate(${hourAngle}deg)`;
+    minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
+    secondHand.style.transform = `rotate(${secondAngle}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
